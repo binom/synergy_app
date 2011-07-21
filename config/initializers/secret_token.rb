590f87a5be33e9bcd8c5fce75087699e66b978eb
binom <binomod@gmail.com>
2011-07-22 00:53:23 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SynergyApp::Application.config.secret_token = 'a8d84abc7202058d995688fe777cba82dadd94c3d6718d4e7325e3a5fbf9bcce5f2284ad76ffa358718886f22634a72f3839005f25beaf8a8b0fb4b31470e95d'
